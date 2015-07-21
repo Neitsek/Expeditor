0e7119895daa457a7bc36327e53ca1cd4ff36029
unknown <Administrateur@26-UC31-13.ad.campus-eni.fr>
2015-07-21 09:59:45 +0200

    s

--- src/com/expeditor/servlets/TraitementCommandeServlet.java
+++ src/com/expeditor/servlets/TraitementCommandeServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class TraitementCommandeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	private static String path = "/employe/TraitementCommande";
+	private static String ServletPath = "/employe/TraitementCommande";
 
     public TraitementCommandeServlet() {
     }
