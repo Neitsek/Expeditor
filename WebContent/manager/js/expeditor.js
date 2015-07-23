25c563d8f2f7669cc8a37e8371536357b89a7cfd
Neitsek <sebastienchesnes@gmail.com>
2015-07-23 11:27:17 +0200

    fixes suiviCommmande

--- WebContent/manager/js/expeditor.js
+++ WebContent/manager/js/expeditor.js
@@ -9,28 +9,21 @@ $( document ).ready(function() {
 		    daysOfWeekDisabled: "0,6"
 		});
 	 
-	 $( "#filtrage" ).submit(function( event ) {
+	/* $( "#filtrage" ).submit(function( event ) {
 		 var employe = $('#selectEmploye option:selected').val();
-		 var listEtat = $("#selectEtat").val();
-		 alert(listEtat);
+		 var listEtat = new Array();
+		 listEtat.push($("#selectEtat").val());
 		 var dateDebut = $('#dateCommande').val();
 		 var dateFin = $('#dateFin').val();
 		 
 		 $.ajax({
 			  method: "POST",
-			  url: "/ReloadSuiviCommandeAjaxServlet",
-			  data: { dateDebut: dateDebut, dateFin: dateFin, idEmploye: employe, listeEtat :listEtat }
+			  url: "${pageContext.request.contextPath}/ReloadSuiviCommandeAjaxServlet",
+			  data: { "dateDebut": dateDebut, "dateFin": dateFin, "idEmploye": employe, "listeEtat" :listEtat }
 			})
 			  .done(function( msg ) {
 			    alert( "Data Send");
 			});
-	});
+	});*/
 	 
-
-	 $('#tableCommande').DataTable({
-		 "dom": '<"top"i>rt<"bottom"flp><"clear">',
-		 "oLanguage": {
-		        "sEmptyTable":     "My Custom Message On Empty Table"
-		  }
-	 });
 });
--- src/com/expeditor/servlets/SuiviCommandeEmploye.java
+++ src/com/expeditor/servlets/SuiviCommandeEmploye.java
@@ -1,6 +1,9 @@
 package com.expeditor.servlets;
 
 import java.io.IOException;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
@@ -10,7 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.expeditor.bll.Connexion;
+import com.expeditor.bo.Commande;
 import com.expeditor.bo.Employe;
+import com.expeditor.dal.CommandeDB;
 import com.expeditor.dal.EmployeDB;
 
 /**
@@ -45,12 +50,53 @@ public class SuiviCommandeEmploye extends HttpServlet {
 	
 	protected void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//vérification de l'etat de la session
-		if(Connexion.verifSession(request.getSession().getAttribute("login")) == true){
+		if(Connexion.verifSession(request.getSession().getAttribute("login")) == true){	
+			//si la page est appelé pour la premiere fois
 			ArrayList<Employe> listEmploye = new ArrayList<Employe>();
 			listEmploye = EmployeDB.getAllEmployeWithoutManager();
-			System.out.println(listEmploye);
 			request.setAttribute("listeEmploye", listEmploye);
 			
+			//si la page est appelé pour un filtrage
+			if(request.getParameter("reload") != null){
+				System.out.println(request.getParameter("etatSel"));
+				Date dateDeb = null;
+				Date dateF = null;
+				String dateDebut = null;
+				String dateFin = null;
+				SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+				if(request.getParameter("dateDebut") != ""){
+					dateDebut = request.getParameter("dateDebut");
+				}
+				if(request.getParameter("dateFin") != ""){
+					dateFin = request.getParameter("dateFin");
+				}
+				try {
+					if(dateDebut != ""){
+						dateDeb = formatter.parse(dateDebut);
+					}
+					if(dateFin != ""){
+						dateF = formatter.parse(dateFin);
+					}
+			 
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				ArrayList<String> listeEtat = new ArrayList<String>();
+				for (String etat : request.getParameterValues("etatSel")){
+					listeEtat.add(etat);
+				}
+				System.out.println(listeEtat); 
+				System.out.println(dateDeb);
+				System.out.println(dateF);
+				System.out.println(Integer.parseInt(request.getParameter("employeSel")));
+				
+				ArrayList<Commande> listeCommande = CommandeDB.selectCommandes(listeEtat, dateDeb, dateF, Integer.parseInt(request.getParameter("employeSel")));
+				
+				System.out.println(listeCommande);
+				if (listeCommande != null){
+					request.setAttribute("listeCommande", listeCommande);
+				}
+			}
 			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/manager/suiviCommande.jsp");
 			requestDispatcher.forward(request, response) ;
 		}else{
